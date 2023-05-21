32068d05173a57273b13fef82ac419a62cbad08a
Siddharth228 <sasiddharth7@gmail.com>
2023-05-21 17:57:27 +0530

    feat: Export dialog and metadata added

--- apps/drawing-app/pages/_document.tsx
+++ apps/drawing-app/pages/_document.tsx
@@ -14,6 +14,33 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="author" content="Siddharth Agrawal" />
+        <meta content="Drawing App" name="title" />
+        <meta content="An intuitive app for drawing" name="description" />
+        <meta
+          name="keywords"
+          content="drawing, canvas, path, strokes, png, jpeg, doodle"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Free online Drawing App" />
+        <meta
+          property="og:description"
+          content="An intuitive app for drawing"
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/monorepo-projects.appspot.com/o/Drawing_Meta_Image.PNG?alt=media"
+        />
+        <meta property="og:image:width" content="1080" />
+        <meta property="og:image:height" content="720" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32, 64x64"
+          href="/squiggle.png"
+        />
       </Head>
       <body>
         <Main />
