deef8d85c5ed545416051975367d20016a538627
Siddharth228 <sasiddharth7@gmail.com>
2023-06-04 21:23:51 +0530

    fix[backend]: cors issue

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3333;
 
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({ allowedHeaders: '*', origin: '*' });
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/}`);
 }
